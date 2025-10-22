e0e110a73f41ad78794ba41ff493d73afc2fa452
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-10-22 17:00:37 -0500

    Add files via upload

---
+++ src/main/java/com/inventatrack/platform/InventatrackApplication.java
@@ -0,0 +1,15 @@
+package com.inventatrack.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class InventatrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventatrackApplication.class, args);
+    }
+
+}
