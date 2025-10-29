978e9a28d2ff2714852598c79c88ae0ad79dfdc2
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-10-28 21:30:11 -0500

    Update UserNotFoundException.java

--- src/main/java/com/inventatrack/platform/users/domain/model/exceptions/UserNotFoundException.java
+++ src/main/java/com/inventatrack/platform/users/domain/model/exceptions/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.inventatrack.platform.users.domain.model.exceptions;
 
-public class UserNotFoundException extends RuntimeException {
+public class UserNotFoundException extends BusinessException {
     public UserNotFoundException(Long id) {
-        super("Usuario con ID " + id + " no encontrado");
+        super("Usuario no encontrado con ID: " + id);
     }
 }
