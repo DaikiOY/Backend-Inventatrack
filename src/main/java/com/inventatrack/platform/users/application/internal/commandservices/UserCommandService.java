a87bbb5b41e8633adf82a82580c768a601309271
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-10-28 21:27:15 -0500

    Update UserCommandService.java

--- src/main/java/com/inventatrack/platform/users/application/internal/commandservices/UserCommandService.java
+++ src/main/java/com/inventatrack/platform/users/application/internal/commandservices/UserCommandService.java
@@ -2,7 +2,12 @@ package com.inventatrack.platform.users.application.internal.commandservices;
 
 import com.inventatrack.platform.users.domain.model.aggregates.User;
 import com.inventatrack.platform.users.domain.model.commands.CreateUserCommand;
+import com.inventatrack.platform.users.domain.model.commands.UpdateUserCommand;
+import com.inventatrack.platform.users.domain.model.commands.UpdateUserPasswordCommand;
 
 public interface UserCommandService {
     User handle(CreateUserCommand command);
+    User handle(UpdateUserCommand command);
+    User handle(UpdateUserPasswordCommand command);
+    void deleteById(Long id);
 }
