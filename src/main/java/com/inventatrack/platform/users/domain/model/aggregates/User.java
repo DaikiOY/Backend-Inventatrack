bbe9d5f3e97665c3913b51528dd666c8a0da15e2
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-10-28 21:28:48 -0500

    Update User.java

--- src/main/java/com/inventatrack/platform/users/domain/model/aggregates/User.java
+++ src/main/java/com/inventatrack/platform/users/domain/model/aggregates/User.java
@@ -1,12 +1,21 @@
 package com.inventatrack.platform.users.domain.model.aggregates;
 
 import jakarta.persistence.*;
+import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import java.time.OffsetDateTime;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,61 +28,5 @@ public class User {
     private String role;
     private String url;
     private OffsetDateTime createdAt;
-
-    public User(String username, String email, String fullName, String password,
-                String phone, String address, String role, String url, OffsetDateTime createdAt) {
-        this.username = username;
-        this.email = email;
-        this.fullName = fullName;
-        this.password = password;
-        this.phone = phone;
-        this.address = address;
-        this.role = role;
-        this.url = url;
-        this.createdAt = createdAt;
-    }
-
-    public User() {}
-
-    // Getters 
-    public Long getId() { return id; }
-
-    public String getUsername() { return username; }
-
-    public String getEmail() { return email; }
-
-    public String getFullName() { return fullName; }
-
-    public String getPassword() { return password; }
-
-    public String getPhone() { return phone; }
-
-    public String getAddress() { return address; }
-
-    public String getRole() { return role; }
-
-    public String getUrl() { return url; }
-
-    public OffsetDateTime getCreatedAt() { return createdAt; }
-
-    // Setters
-    public void setId(Long id) { this.id = id; }
-
-    public void setUsername(String username) { this.username = username; }
-
-    public void setEmail(String email) { this.email = email; }
-
-    public void setFullName(String fullName) { this.fullName = fullName; }
-
-    public void setPassword(String password) { this.password = password; }
-
-    public void setPhone(String phone) { this.phone = phone; }
-
-    public void setAddress(String address) { this.address = address; }
-
-    public void setRole(String role) { this.role = role; }
-
-    public void setUrl(String url) { this.url = url; }
-
-    public void setCreatedAt(OffsetDateTime createdAt) { this.createdAt = createdAt; }
 }
+
